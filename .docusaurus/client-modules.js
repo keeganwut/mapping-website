071e68089280fef0f3fdebbf0027673a3050bc82
PeeganKreece <boytrek991@gmail.com>
2023-06-24 04:46:42 -0400

    Remove Junk

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Projects\\Websites\\Docs\\mapping-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Projects\\Websites\\Docs\\mapping-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Projects\\Websites\\Docs\\mapping-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Projects\\Websites\\Docs\\mapping-website\\src\\css\\custom.css'),
-];
